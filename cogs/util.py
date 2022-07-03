6f5e5bdc249226595eefd290a38b2515a427c83f
KristopherPeck <Kristopher.R.Peck@gmail.com>
2022-07-03 01:41:55 -0400

    Formatting and Command Additions
    Added randombaki and diceroll commands. Move some stuff around like help to the top.

--- cogs/util.py
+++ cogs/util.py
@@ -1,4 +1,5 @@
 import discord
+import sys
 import random
 from discord.ext import commands
 from discord.ext.commands import bot
@@ -21,16 +22,41 @@ gameList = [
             "Tom Clancy's The Division 2",
             'Zombie Army Trilogy'
         ]
+        
+bakiQuotes = [
+            "You'd do or say anything to save your skin... probably even lick my ass when nobody was looking. -Sikorsky",
+            "Now that you've got no more urine left in you... How are you going to get out of this tetrahedron? -Mouth",
+            "It seems that in this fight... I won't be able to win this without pissing on myself just a little bit. -Baki Hanma",
+            "I started to wish I didn't have any fingers.. because then I could punch all out. My dream just came true. -Doppo 'Tiger Slayer' Orochi",
+            "His tuxedo is still there...!! Just like a lesson in shedding your skin!!",
+            "What's futile is not realizing the reality of your own futility. One hundred cowards are the same as one. -Yujiro 'The Ogre' Hanma"
+        ]
 
 class Util(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    @commands.command(name="help")
+    async def DM(self, ctx):
+        embed=discord.Embed(
+            title="Command List",
+            color=discord.Color.blue())
+        embed.add_field(name="**choose**", value="Pick a random option from the items given. Put a space between each item like this: !choose Lead Salt Diesel", inline=False)
+        embed.add_field(name="**diceroll**", value="Rolls a die of your choosing. Just like this: !diceroll 20", inline=False)
+        embed.add_field(name="**gamelist**", value="Shows the current game list", inline=False)
+        embed.add_field(name="**randombaki**", value="Posts a random quote from Baki", inline=False)
+        embed.add_field(name="**randomgame**", value="Pick a random game from the full list of games we own", inline=False)
+        await ctx.author.send(embed=embed)
         
     @commands.command(name='randomgame', help='Randomly picks a game from the server side game list')
     async def randomgame(self, ctx):
         response = random.choice(gameList)
         await ctx.send("You should play: " + response)
+        
+    @commands.command(name='randombaki')
+    async def randombaki(self, ctx):
+        response = random.choice(bakiQuotes)
+        await ctx.send(response)
         
     @commands.command(name="choose", description="Choose from a list", usage="choose <item1 item2 item3 ... >")
     async def choose(self, ctx, *args):
@@ -39,23 +65,18 @@ class Util(commands.Cog):
 
     @commands.command(name="gamelist", description="Shows the current game list")
     async def on_message(self, ctx):
-        newList = '**Current Game list**'
+        newList = '**Current Game list**' + '\n'
         for game in gameList:
             newList = newList + '\n' + game
         
         await ctx.send(newList)
-
-    @commands.command(name="help")
-    async def DM(self, ctx):
-        embed=discord.Embed(
-        title="Command List",
-            color=discord.Color.blue())
-        embed.add_field(name="**randomgame**", value="Pick a random game from the full list of games we own", inline=False)
-        embed.add_field(name="**choose**", value="Pick a random option from the items given. Put a space between each item like this: !choose Lead Salt Diesel", inline=False)
-        embed.add_field(name="**gamelist**", value="Shows the current game list", inline=False)
-
-        await ctx.author.send(embed=embed)
+        
+    @commands.command(name="diceroll")
+    async def diceroll(self, ctx, arg1):
+        arg1 = int(arg1)
+        result = random.randint(1, arg1)
+        result = str(result)
+        await ctx.send("Show me the money!: " + result)
     
 def setup(bot):
     bot.add_cog(Util(bot))
-    
