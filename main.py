0291e5d83880846ac4c3973260844435f20deda1
KristopherPeck <Kristopher.R.Peck@gmail.com>
2023-02-19 11:28:04 -0500

    Added checkcogs
    Added the checkcogs command to try and reload all possible cogs and report if they are currently unloaded or not. Also added some very basic comments.

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ bot.remove_command('help')
 
 @bot.event
 async def on_ready():
+    #Set discord presence
     await bot.change_presence(activity=discord.Game(name="!help"))
 
 @bot.event
 async def on_command_error(ctx, error):
+    #Configure some error messages so we get some idea what something is broken
     if isinstance(error, commands.CommandNotFound):
         await ctx.send("That command does not exist. Please check !help for the current commands")
     elif isinstance(error, commands.MissingRequiredArgument):
@@ -27,19 +29,39 @@ async def on_command_error(ctx, error):
         
 @bot.command()
 async def load(ctx, extension):
+    #Load an unloaded cog
     bot.load_extension(f'cogs.{extension}')
     await ctx.send("Loaded Cog")
 
 @bot.command()
 async def unload(ctx, extension):
+    #unload loaded cog
     bot.unload_extension(f'cogs.{extension}')
     await ctx.send("Unloaded Cog")
 
 @bot.command()
 async def reload(ctx, extension):
+    #reload loaded cog
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
     await ctx.send("Reloaded Cog")
+
+@bot.command()
+async def checkcogs(ctx):
+    #Check current status of all cogs and attempt to reload any unloaded cogs
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            try:
+                bot.load_extension(f'cogs.{filename[0:-3]}')
+            except commands.ExtensionAlreadyLoaded:
+                await ctx.send({filename[0:-3]})
+                await ctx.send("is loaded")
+            except commands.ExtensionNotFound:
+                await ctx.send({filename[0:-3]})
+                await ctx.send("is not found")
+            else:
+                await ctx.send({filename[0:-3]})
+                await ctx.send("is unloaded")
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
