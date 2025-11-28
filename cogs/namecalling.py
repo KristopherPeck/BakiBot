2a8df0bbf4bfe9c8b408c7eb721fde0adfb1d58a
KristopherPeck <21282789+KristopherPeck@users.noreply.github.com>
2025-11-28 12:52:53 -0500

    Update namecalling.py

--- cogs/namecalling.py
+++ cogs/namecalling.py
@@ -2,6 +2,7 @@ import discord
 import sys
 import random
 from discord.ext import commands
+from discord import app_commands
 from discord.ext.commands import bot
 from discord.ext.commands import Context
 
@@ -16,12 +17,13 @@ class NameCalling(commands.Cog):
         c = discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         await ctx.send(embed=discord.Embed(description=response + " is a scrub!", colour=c))
         
-    @commands.command(name="findthem")
-    @commands.cooldown(1.0,3.0)
-    async def on_message(self, ctx):
-        users = [random.choice(ctx.guild.members)]       
+    @app_commands.command(name="findthem", description="Pick a random person from the server to call a scrub.")
+    @app_commands.checks.cooldown(1, 3)
+    async def findthem(self, interaction: discord.Interaction):
+        users = [random.choice(interaction.guild.members)]       
         c = discord.Color.from_rgb(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        await ctx.send(embed=discord.Embed(description=f"{users[0].mention} is a scrub!", colour=c))
+        embed = discord.Embed(description=f"{users[0].mention} is a scrub!", colour=c)
+        await interaction.response.send_message(embed=embed)
     
 async def setup(bot):
     await bot.add_cog(NameCalling(bot))
